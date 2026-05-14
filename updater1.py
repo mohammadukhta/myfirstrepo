8999954159b2fb6e78cc78622aabc2e5eba4d1f2
mohammadukhta <derbent_uhta@proton.me>
2026-05-14 23:03:36 +0300

    Delete updater1.py

--- updater1.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from pathlib import Path
-
-# проверка существования файла с паролем
-path = Path("update1.txt")
-    
-print("Файл существует" if path.exists() else "Файл не существует")
